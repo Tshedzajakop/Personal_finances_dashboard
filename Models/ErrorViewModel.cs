323980e91fab5d4b5977f5c11a02e4c92485225c
Tshedzajakop <tshedza.jakop@gmail.com>
2025-07-29 16:32:20 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Personal_finances_dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
